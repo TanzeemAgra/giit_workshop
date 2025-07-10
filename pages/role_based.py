2783b5c8a6ceb1c3d95299c53a4479126fcecc25
Tanzeem <info@xerxez.in>
2025-07-10 14:27:10 +0530

    Fifth

--- pages/role_based.py
+++ pages/role_based.py
@@ -9,3 +9,9 @@ load_dotenv()
 #add the parent directory to the path to import utils
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils import get_openai_response
+
+#Role-Based System Prompts 
+ROLE_PROMPTS = {
+    "Default": "You are a helful assistant",
+    "Teacher" : "You are an experienced and patient school teacher who explain concepts clearly with examples and encourage learning. Use simple language and break down complex topics into easy-to-understand parts",
+}
