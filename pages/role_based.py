4d59854371d44bf1ce3be4341ebbbf3bb384f444
Tanzeem <info@xerxez.in>
2025-07-10 14:32:13 +0530

    Sixth

--- pages/role_based.py
+++ pages/role_based.py
@@ -14,4 +14,5 @@ from utils import get_openai_response
 ROLE_PROMPTS = {
     "Default": "You are a helful assistant",
     "Teacher" : "You are an experienced and patient school teacher who explain concepts clearly with examples and encourage learning. Use simple language and break down complex topics into easy-to-understand parts",
+    "Doctor": "You are a professional medical doctor who provides advice based on symptoms. Always remaind users to consult with a real healthcare provider for serious concerns. Be Be informative but responsible,",
 }
