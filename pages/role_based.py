b0066a94f9cf21f448ac5e0c43dddaf7246bb326
Tanzeem <info@xerxez.in>
2025-07-10 14:19:05 +0530

    Second Commit

---
+++ pages/role_based.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import sys
+import os
+from dotenv import load_dotenv
+
+#Load Environemtn Variable
+load_dotenv()
+
+#add the parent directory to the path to import utils
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
