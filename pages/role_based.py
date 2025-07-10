476ca7553903a05efb65183dc2dc0cf94dfcd511
Tanzeem <info@xerxez.in>
2025-07-10 14:24:12 +0530

    Third Commit

--- pages/role_based.py
+++ pages/role_based.py
@@ -8,3 +8,4 @@ load_dotenv()
 
 #add the parent directory to the path to import utils
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils import get_openai_response
